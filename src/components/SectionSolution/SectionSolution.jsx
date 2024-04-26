17efb324d1ae5eaec733b8b14112d6f8d8fea2bb
aliskhangazamatov <ag@farcana.com>
2024-04-26 23:56:12 +0300

    add block

---
+++ src/components/SectionSolution/SectionSolution.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function SectionSolution() {
+  return (
+    <div>
+      <div className="max-w-[580px] mx-auto">
+        <h2 className="title-section mb-5 text-center">
+          Innovative solutions for operational excellence
+        </h2>
+        <p className="subtitle-section text-center">
+          Tailored DevOps expertise driving your business momentum. Personalized
+          solutions for growth and evolution.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SectionSolution;

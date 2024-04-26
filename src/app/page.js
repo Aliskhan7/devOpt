f8bcdd5978568f63b0e0e5077fa4bea39e7c9685
aliskhangazamatov <ag@farcana.com>
2024-04-26 19:32:06 +0300

    add block

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from "@/components/Header/Header";
 import Main from "@/components/Main/Main";
+import SectionDevoptima from "@/components/SectionDevoptima/SectionDevoptima";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Main />
+        <SectionDevoptima/>
     </div>
   );
 }
